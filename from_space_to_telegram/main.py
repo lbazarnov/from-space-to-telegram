11c02cb4aee7575781dc97a7ecb134072765a47d
Lev Bazarnov <16255087+lbazarnov@users.noreply.github.com>
2023-01-02 21:46:58 +0400

    added fetch_nasa_apod function

--- from_space_to_telegram/main.py
+++ from_space_to_telegram/main.py
@@ -1,30 +1,56 @@
 import requests
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+from urllib.parse import urlsplit, unquote
+
+NUMBER_OF_NASA_PHOTOS = 35
 
 
-def get_images_urls_list(url):
-    response = requests.get(url)
+def download_image(url, path, params=None):
+    response = requests.get(url, params=params)
     response.raise_for_status()
-    json_response = response.json()
-    spacex_launch_images_urls = json_response['links']['flickr']['original']
-    return spacex_launch_images_urls
+    with open(path, 'wb') as file:
+        file.write(response.content)
 
 
-def download_image(urls, path):
-    for image_number, image_url in enumerate(urls):
-        response = requests.get(image_url)
-        response.raise_for_status()
-        filename = f'{path}spacex{image_number}.jpg'
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+def get_file_extension(url):
+    splitted_url = os.path.splitext(urlsplit(url).path)
+    file_extension = splitted_url[1]
+    return file_extension
+
+
+def fetch_spacex_last_launch(path):
+    spacex_launch_images_url = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'  # noqa: E501
+    response = requests.get(spacex_launch_images_url)
+    response.raise_for_status()
+    json_response = response.json()['links']['flickr']['original']
+    for image_number, image_url in enumerate(json_response):
+        file_extension = get_file_extension(image_url)
+        file_path = f'{path}spacex{image_number}{file_extension}'
+        download_image(image_url, file_path)
+
+
+def fetch_nasa_apod(api_key, path):
+    nasa_url = 'https://api.nasa.gov/planetary/apod'
+    payload = {'api_key': api_key,
+               'count': 35}
+    response = requests.get(url=nasa_url, params=payload)
+    response.raise_for_status()
+    image_urls = [apod['url'] for apod in response.json()]
+    for image_number, image_url in enumerate(image_urls):
+        file_extension = get_file_extension(image_url)
+        file_path = f'{path}nasa_apod_{image_number}{file_extension}'
+        download_image(image_url, file_path)
 
 
 def main():
+    load_dotenv()
+    nasa_api_key = os.environ['NASA_API_TOKEN']
     images_path = 'images/'
     Path(f'{images_path}').mkdir(exist_ok=True)
-    spacex_url = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
-    images_urls = get_images_urls_list(spacex_url)
-    download_image(images_urls, images_path)
+    fetch_spacex_last_launch(images_path)
+    fetch_nasa_apod(nasa_api_key, images_path)
 
 
 if __name__ == '__main__':
