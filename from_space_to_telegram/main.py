58ff4dcc81cbe329e1735a89b98823a7f93865da
Lev Bazarnov <16255087+lbazarnov@users.noreply.github.com>
2023-01-03 23:45:44 +0400

    added fetch_nasa_epic_photos function

--- from_space_to_telegram/main.py
+++ from_space_to_telegram/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime
 from pathlib import Path
 from dotenv import load_dotenv
 from urllib.parse import urlsplit
@@ -46,13 +47,30 @@ def fetch_nasa_apod(api_key, path):
         download_image(image_url, file_path)
 
 
+def fetch_nasa_epic_photos(api_key, path):
+    nasa_epic_url = 'https://api.nasa.gov/EPIC/api/natural'
+    payload = {'api_key': api_key}
+    response = requests.get(nasa_epic_url, params=payload)
+    response.raise_for_status()
+    recent_epic_images = response.json()
+    for epic_image in recent_epic_images:
+        epic_image_name = epic_image['image']
+        filename = f'{epic_image_name}.png'
+        image_datetime = datetime.fromisoformat(epic_image['date'])
+        image_date = image_datetime.strftime('%Y/%m/%d')
+        image_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{filename}'  # noqa: E501
+        file_path = f'{path}/nasa_{filename}'
+        download_image(image_url, file_path, params=payload)
+
+
 def main():
     load_dotenv()
     nasa_api_key = os.environ['NASA_API_TOKEN']
     images_path = 'images'
     Path(f'{images_path}').mkdir(exist_ok=True)
-    fetch_spacex_last_launch(images_path)
-    fetch_nasa_apod(nasa_api_key, images_path)
+    # fetch_spacex_last_launch(images_path)
+    # fetch_nasa_apod(nasa_api_key, images_path)
+    fetch_nasa_epic_photos(nasa_api_key, images_path)
 
 
 if __name__ == '__main__':
