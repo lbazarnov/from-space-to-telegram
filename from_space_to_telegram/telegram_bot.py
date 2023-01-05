a19182b3a8d8cec67f15374cbd02df074eddba7f
Lev Bazarnov <16255087+lbazarnov@users.noreply.github.com>
2023-01-06 02:17:44 +0400

    published photo to telegram channel

--- from_space_to_telegram/telegram_bot.py
+++ from_space_to_telegram/telegram_bot.py
@@ -6,4 +6,4 @@ load_dotenv()
 telegram_api_token = os.environ['TELEGRAM_API_TOKEN']
 channel_id = os.environ['CHANNEL_ID']
 bot = telegram.Bot(token=telegram_api_token)
-bot.send_message(text='Hello', chat_id=channel_id)
+bot.send_photo(chat_id=channel_id, photo=open('spacex0.jpg', 'rb'))
