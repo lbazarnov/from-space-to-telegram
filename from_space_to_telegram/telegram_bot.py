c74d761f5eaea7a78d9f46d85358b862560d689d
Lev Bazarnov <16255087+lbazarnov@users.noreply.github.com>
2023-03-25 19:14:17 +0400

    Implemented feature for automatic image posting to Telegram channel

--- from_space_to_telegram/telegram_bot.py
+++ from_space_to_telegram/telegram_bot.py
@@ -1,14 +1,27 @@
 import os
-import time
 import telegram
 from dotenv import load_dotenv
+from random import shuffle
+from time import sleep
+from argparse import ArgumentParser
 
 
-def send_images_to_telegram(api_token, channel_id, path):
+def send_images_to_telegram(api_token, channel_id, images_directory, delay):
+    '''Sends images to Telegram channel'''
     bot = telegram.Bot(token=api_token)
-    root, dirs, files = os.walk(path)
-    for image in files:
-        bot.send_photo(chat_id=channel_id, photo=open(f'{image}', 'rb'))
+    images = []
+    path_to_directory = os.path.join(os.getcwd(), images_directory)
+    for subdirectory in os.walk(path_to_directory):
+        base, _, files = subdirectory
+        paths = [os.path.join(base, file) for file in files]
+        images.extend(paths)
+    while True:
+        for image in images:
+            with open(image, 'rb') as image:
+                if os.path.getsize(image.name)/(1024**2) < 20:
+                    bot.send_document(channel_id, image)
+        shuffle(images)
+        sleep(delay)
 
 
 def main():
@@ -16,7 +29,16 @@ def main():
     telegram_api_token = os.environ['TELEGRAM_API_TOKEN']
     channel_id = os.environ['CHANNEL_ID']
     path = 'images'
-    send_images_to_telegram(telegram_api_token, channel_id, path)
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(nargs='?',
+                            dest='pause_time',
+                            help='Time interval for posting photos to the channel in seconds',  # noqa: E501
+                            default=4*60*60,
+                            type=int)
+    args = arg_parser.parse_args()
+    delay = args.pause_time
+    send_images_to_telegram(
+        telegram_api_token, channel_id, path, delay)
 
 
 if __name__ == '__main__':
